ca59cfd48cd789f8d03af5fcaa271b5b4e65835e
Matthias Uschok <dev@uschok.de>
2020-12-29 15:52:10 +0100

    Relax version of Python dependency from 3.8 to 3.7

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name="nfclock",
-    version="1.0",
+    version="1.0.1",
     author="Steffen Arntz, Matthias Uschok",
     author_email="dev@uschok.de",
     description="Simple tool to identify and authenticate cheap nfc-tags against a DB of known authorized keys.",
     url="https://github.com/hellfyre/nfclock",
     packages=setuptools.find_packages(),
-    python_requires='>=3.8'
+    python_requires='>=3.7'
 )
